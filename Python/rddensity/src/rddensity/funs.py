ddff41aaba35bb35672765d52991d637fb5eb889
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2023-05-15 14:43:26 -0400

    working on pandas loc fix

--- Python/rddensity/src/rddensity/funs.py
+++ Python/rddensity/src/rddensity/funs.py
@@ -27,10 +27,10 @@ def __rddensityUnique(x):
         return({'unique':None, 'freq':[], 'indexFirst':[], 'indexLast':[]})
     if n==1:
         return({'unique':x, 'freq': 1, 'indexFirst':0, 'indexLast':0})
-    
+
     unique, uniqueIndex, nUniquelist = np.unique(x, return_index=True, return_counts=True, axis=0)
     nUnique = sum(unique)
-    
+
     return({'unique':unique, 'freq' : nUniquelist, 'indexFirst':uniqueIndex, 'indexLast': uniqueIndex+nUniquelist-1})
 
 
@@ -46,9 +46,9 @@ def __Sgenerate(p, low=-1, up=1, kernel='triangular'):
                 integrand = lambda l: 0.75*np.power(l, i+j-2)*(1-l**2)
             else:
                 integrand = lambda l: (1-abs(l))*np.power(l, i+j-2)
-            
+
             S[i-1, j-1] = integrate.quad(integrand, low, up)[0]
-            
+
     return(S)
 
 
@@ -81,9 +81,9 @@ def __Cgenerate(k, p, low=-1, up=1, kernel='triangular'):
             integrand = lambda l: 0.75*np.power(l, i+k-1)*(1-l**2)
         else:
             integrand = lambda l: (1-abs(l))*np.power(l, i+k-1)
-        
+
         C[i-1, 0] = integrate.quad(integrand, low, up)[0]
-        
+
     return(C)
 
 
@@ -101,7 +101,7 @@ def __Cplusgenerate(k, p, kernel='triangular'):
     temp = np.zeros((p+2, 1))
     # insert row of zeros
     temp = np.insert(C, 1, np.zeros((1, 1)), 0)
-    
+
     return(temp)
 
 # Generate G matrix
@@ -112,48 +112,48 @@ def __Ggenerate(p, low=-1, up=1, kernel='triangular'):
             if kernel=='uniform':
                 def integrand_1(x,y):
                     return(np.power(x,i) * np.power(y, (j-1))*0.25)
-            
+
                 def integrand_2(x,y):
                     return(np.power(x,i-1) * np.power(y, j)*0.25)
-                
+
                 def x_integral_1(y):
                     return (integrate.quad(integrand_1, low, y, args=(y))[0])
-                
+
                 def x_integral_2(y):
                     return (integrate.quad(integrand_2, y, up, args=(y))[0])
-                
+
                 G[i-1, j-1] = integrate.quad(x_integral_1, low, up)[0] + integrate.quad(x_integral_2, low, up)[0]
-                
+
             elif kernel == 'epanechnikov':
                 def integrand_1(x,y):
                     return(np.power(x,i) * np.power(y, (j-1))*np.power(0.75, 2)*(1-x**2)*(1-y**2))
-                
+
                 def integrand_2(x,y):
                     return(np.power(x,i-1) * np.power(y, j)*np.power(0.75, 2)*(1-x**2)*(1-y**2))
-                
+
                 def x_integral_1(y):
                     return (integrate.quad(integrand_1, low, y, args=(y))[0])
-                
+
                 def x_integral_2(y):
                     return (integrate.quad(integrand_2, y, up, args=(y))[0])
-                
+
                 G[i-1, j-1] = integrate.quad(x_integral_1, low, up)[0] + integrate.quad(x_integral_2, low, up)[0]
-                
+
             elif kernel=='triangular':
                 def integrand_1(x,y):
                     return(np.power(x,i) * np.power(y, (j-1))*(1-abs(x))*(1-abs(y)))
-                
+
                 def integrand_2(x,y):
                     return(np.power(x,i-1) * np.power(y, j)*(1-abs(x))*(1-abs(y)))
-                
+
                 def x_integral_1(y):
                     return (integrate.quad(integrand_1, low, y, args=(y))[0])
-                
+
                 def x_integral_2(y):
                     return (integrate.quad(integrand_2, y, up, args=(y))[0])
-                
+
                 G[i-1, j-1] = integrate.quad(x_integral_1, low, up)[0] + integrate.quad(x_integral_2, low, up)[0]
-        
+
     return(G)
 
 
@@ -181,7 +181,7 @@ def __Psigenerate(p):
         temp = np.append(np.array([1, 0, 0]), np.power(-1, range(2, p+1)))
     else:
         temp = np.array([1,0,0])
-        
+
     temp = np.diag(temp)
     temp[1,2] = temp[2,1] = -1
     return(temp)
@@ -191,7 +191,7 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
     n = nl + nr
     nh = nlh +nrh
 
-    
+
     # Construct kernel weights
     W = np.repeat(np.nan, nh)
     W = pd.DataFrame(W)
@@ -204,7 +204,7 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
     else:
         W[0:(nlh)] = 0.75*((1 + X[0:(nlh)]/hl)**2)/hl
         W[nlh:(nh)] = 0.75*((1 + X[nlh:(nh)]/hr)**2)/hr
-        
+
     # Construct the design matrix and the bandwidth matrix
     if fitselect=='restricted':
         if nh ==0:
@@ -216,7 +216,7 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
             Xp[nlh:nh, 1] = 0
             Xp[0:(nlh), 2] = 0
             Xp[nlh:nh, 2] = (X[nlh:nh]/hr)[X.columns[0]]
-            
+
             if p>1:
                 for j in range(3, p+2):
                     Xp[0:(nlh), j] = np.power(X[0:(nlh)]/hl, j-1)[X.columns[0]]
@@ -241,25 +241,28 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
                     Xp[nlh:(nh), j-1] = np.power(X[nlh:(nh)]/hr, (j-2)/2)[X.columns[0]]
                     Hp[j-1] = np.power(hr, (j-2)/2)
             Hp = np.diag(Hp)
-            
+
     out = np.full((4,4), np.nan)
-            
+
     #X'WX inverse matrix
     Xp = pd.DataFrame(Xp)
     ncol = len(Xp.columns)
     nrow = len(Xp)
     XpW = pd.DataFrame(np.zeros((nrow, ncol)))
+    print(np.multiply(Xp[[1]], W))
+    print(np.multiply(Xp[[2]], W))
     for j in range(ncol):
+        print(j)
         XpW[[j]] = np.multiply(Xp[[j]], W)
 
     try:
         Sinv = inv(np.matmul(XpW.T, Xp))
     except:
         return(out)
-    
+
     # point estimates
     b = np.matmul(inv(Hp), np.matmul(Sinv, np.matmul(XpW.T, Y)))
-    
+
     if fitselect=='restricted':
         out[0, 0] = b[0][1]
         out[1, 0] = b[0][2]
@@ -278,7 +281,7 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
         out[1, 3] = b[0][2*s+1]
         out[2, 3] = out[1, 3] - out[0, 3]
         out[3, 3] = out[1, 3] + out[0, 3]
-        
+
     # Jackknife
     if vce=='jackknife':
         L = np.full((nrow, ncol), None)
@@ -305,7 +308,7 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
             out[1, 1] = V[3, 3]
             out[2, 1] = V[2, 2] + V[3, 3] - 2*V[2, 3]
             out[3, 1] = V[2, 2] + V[3, 3] + 2*V[2, 3]
-        
+
     # plug in
     if vce=='plugin':
         if fitselect=='unrestricted':
@@ -322,19 +325,19 @@ def __rddensity_fv(Y, X, nl, nr, nlh, nrh, hl, hr, p, s, kernel, fitselect, vce,
             Psi = __Psigenerate(p=p)
             Sm = np.matmul(Psi, np.matmul(S, Psi))
             Gm = np.matmul(Psi, np.matmul(G, Psi))
-            
+
             V = np.matmul(np.matmul(inv(out[0, 0] * Sm + out[1, 0]*S), np.power(out[0, 0], 3)*Gm + np.power(out[1, 0], 3)*G), inv(out[0, 0]*Sm + out[1, 0]*S))
             out[0, 2] = V[1, 1]/(n*hl)
             out[1, 2] = V[2, 2]/(n*hl)
             out[2, 2] = (V[1, 1] + V[2, 2] - 2*V[1, 2])/(n*hl)
             out[3, 2] = (V[1, 1] + V[2, 2] + 2*V[1, 2])/(n*hl)
-    
+
     for i in range(4):
         for j in range(1, 3):
             if not np.isnan(out[i, j]):
                 if out[i, j]<0:
                     out[i, j] = np.nan
-                    
+
     out = pd.DataFrame(out, columns=('hat', 'jackknife', 'plugin', 's'), index=('l', 'r', 'diff', 'sum'))
     return(out)
 
@@ -343,28 +346,28 @@ def __h_opt_density(x, p, n, dgp_f1, dgp_fp1, f_low, f_up, kernel='triangular'):
     if kernel != 'triangular' & kernel!='epanechnikov' & kernel != 'uniform':
         warnings.warn("invalid kernel input given. Using triangular kernel instead.")
         kernel = 'triangular'
-        
+
     if x==f_low | x==f_up:
         c_low = 0
     else:
         c_low = -1
-        
+
     c_up = 1
-    
+
     e_mat = np.zeros((p+1, 1))
     e_mat[1] = 1
-    
+
     S1 = __Sgenerate(p=p, low=c_low, up=c_up, kernel=kernel)
     Cp1 = __Cgenerate(k=p+1, p=p, low = c_low, up=c_up, kernel=kernel)
     G =__Ggenerate(p=p, low=c_low, up=c_up, kernel=kernel)
-    
+
     kappa = np.power(n, -1/(2*p+1)) * np.power(np.matmul(np.transpose(e_mat), np.matmul(inv(S1), np.matmul(G, np.matmul(inv(S1), e_mat)))), 1/(2*p+1)) * np.power(np.matmul(abs(np.transpose(e_mat)), np.matmul(inv(S1), Cp1)), -2/(2*p+1)) * np.power(math.factorial(p+1), 2/(2*p+1)) * np.power(2*p, -1/(2*p+1))
-        
+
     biassq = np.power(abs(dgp_fp1), -2/(2*p+1))
     var1 = np.power(dgp_f1, 1/(2*p+1))
-    
+
     h_opt = kappa * biassq * var1
-    
+
     return(h_opt)
 
 
@@ -373,7 +376,7 @@ def __h_opt_density_res(p, n, dgp_f1_l, dgp_f1_r, dgp_fp1_l, dgp_fp1_r, kernel='
     if kernel != 'triangular' & kernel!='epanechnikov' & kernel != 'uniform':
         warnings.warn("invalid kernel input given. Using triangular kernel instead.")
         kernel = 'triangular'
-    
+
     e_mat = np.zeros((p+2, 1))
     if p%2:
         e[1] = -1
@@ -381,15 +384,15 @@ def __h_opt_density_res(p, n, dgp_f1_l, dgp_f1_r, dgp_fp1_l, dgp_fp1_r, kernel='
     else:
         e[1] = 1
         e[2] = 1
-    
+
     Sminus = __Sminusgenerate(p=p, kernel=kernel)
     Splus = __Splusgenerate(p=p, kernel=kernel)
     Sinv = inv(Sminus*dgp_f1_l+ Splus*dgp_f1_r)
     C = dgp_f1_l * dgp_f1_l * __Cminusgenerate(k=p+1, p=p, kernel=kernel) + dgp_f1_r * dgp_f1_r * __Cplusgenerate(k=p+1, p=p, kernel=kernel)
     G = np.power(dgp_f1_l, 3) * __Gminusgenerate(p=p, kernel=kernel) + np.power(dgp_f1_r, 3) * __Gplusgenerate(p=p, kernel=kernel) + np.power(dgp_f1_l, 2) * dgp_f1_r* np.matmul(Sminus[1, :], Splus[0, :]) + np.power(dgp_f1_l, 2) * dgp_f1_r* np.matmul(Splus[0, :], Sminus[1, :])
-    
+
     h_opt = np.power(n, -1/(2*p+1)) * np.power(np.matmul(e_mat, Sinv) * np.matmul(G, np.matmul(Sinv, e_mat)), 1/(2*p+1))* np.power(np.matmul(abs(e_mat), np.matmul(Sinv, C)), -2/(2*p+1)) * np.power(math.factorial(p+1), 2/(2*p+1)) * np.power(2*p, -1/(2*p+1))
-    
+
     return(h_opt)
 
 def __rddensity_H(x, p):
--- Python/rddensity_illustration.py
+++ Python/rddensity_illustration.py
@@ -13,7 +13,7 @@ print(repr(rddensity.rddensity(X=data, useall=True)))
 
 # rddensity: default statistic, restricted model & plugin standard errors
 print(repr(rddensity.rddensity(X=data, fitselect="restricted", vce="plugin")))
- 
+
 ### rdplotdensity: default options
 rdd = rddensity.rddensity(X=data)
 rddensity.rdplotdensity(rdd, data)
@@ -35,4 +35,3 @@ print(repr(rddensity.rddensity(X=data, c=5, useall=True)))
 
 print(repr(rddensity.rdbwdensity(X=data, p=3, fitselect='restricted')))
 print(repr(rddensity.rdbwdensity(X=data, kernel='uniform', vce='jackknife')))
-
